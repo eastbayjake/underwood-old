5eda74bdc046404aecd97e4d826242e7179c7fb1
Jake McGuire <mcguireghs@gmail.com>
2013-08-10 17:36:52 -0700

    WIP: Closing up for the night

--- js/main.js
+++ js/main.js
@@ -17,6 +17,11 @@ app.controller('LegislatorController', function($scope, $http) {
     });
   }
 
+  $scope.selectVote = function() {
+    // Set all classes to "nonselected"
+    // Add "{{legislator:party}}" class
+  }
+
   //Get list of states to populate the dropdown menu
   $http({
     method: 'JSONP',
