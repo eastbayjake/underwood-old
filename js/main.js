9e782e19f2ee39a53668f5ce90fe9c6f25f09d86
Jake McGuire <mcguireghs@gmail.com>
2013-08-12 21:45:06 -0700

    Massive re-write to dynamically add classes, separate legislators into objects

--- js/main.js
+++ js/main.js
@@ -5,17 +5,37 @@ var apiKey = '3cdfa27b289e4d4090fd1b176c45e6cf';
 app.controller('LegislatorController', function($scope, $http) {
   $scope.state = 'al';
   $scope.chamber = 'lower';
+  $scope.undecidedCount = null;
+
+  var processNewData = function(data) {
+    for (i=0; i<data.length; i++) {
+      var id = data[i]['transparencydata_id'];
+      $scope[id] = {};
+      $scope[id]['id'] = data[i]['transparencydata_id'];
+      $scope[id]['last_name'] = data[i]['last_name'];
+      $scope[id]['party'] = data[i]['party'];
+      $scope[id]['vote'] = 'undecided';
+      $scope.undecidedCount += 1;
+      $scope.legislators.push($scope[id]);
+    };
+  };
+
+  $scope.changeVote = function(id) {
+    $scope[id]['vote'] = 'leanYes';
+  };
 
   var getNewList = function() {
+    $scope.undecidedCount = null;
+    $scope.legislators = [];
     $http({
       method: 'JSONP',
       url: 'http://openstates.org/api/v1/legislators/?state='+$scope.state+'&chamber='+$scope.chamber+'&active=true&apikey='+apiKey+'&callback=JSON_CALLBACK'
     }).success(function(data, status) {
-      $scope.legislators = data;
+      processNewData(data);
     }).error(function(data, status) {
       // Error
     });
-  }
+  };
 
   //Get list of states to populate the dropdown menu
   $http({
